59dc76ec7f90bf0cbe79ee91c1207e59f6693394
Felice H <felichay@outlook.com>
2013-11-02 23:43:46 -0400

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,20 @@
 SampleApp::Application.routes.draw do
-  get "static_pages/home"
+  #get "users/new"
 
-  get "static_pages/help"
+  # Defining named routes, matches URI and routes to each of the actions in the StaticPages controller, can now go directly to /about path instead of /static_pages/about
+  root to: 'static_pages#home'
+  match '/help', to: 'static_pages#help'
+  match '/about', to: 'static_pages#about'
+  match '/contact', to: 'static_pages#contact'
+  match '/signup', to: 'users#new'
 
-  get "static_pages/about"
+  #get "static_pages/home"
 
-  get "static_pages/contact"
+  #get "static_pages/help"
+
+  #get "static_pages/about"
+
+  #get "static_pages/contact"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,14 +1,23 @@
 require 'spec_helper'
 
-describe "StaticPages" do
-
+describe "StaticPages" do  
 =begin
 define base_title in app/helpers/application_helper.rb instead
   let (:base_title) { "Sample App" }
   # describing the Home page (content inside quotes meant to be description, irrelevant to RSpec)
 =end
+  subject { page }
   describe "Home page" do
-=begin
+    # before block allows for elmination of visit to  root path duplication
+    before { visit root_path }
+
+    it { should have_selector('h1',    text: 'Sample App') }
+    it { should have_selector('title', text: full_title('')) }
+    it { should_not have_selector 'title', text: '| Home' }
+  end
+  
+=begin    
+  =begin
     # when you visit the Home page at /static_pages/home, the content should contain the words "Sample App"
     it "should have the content 'Sample App'" do
     	# using Capybara to similate visiting the URI
@@ -16,16 +25,18 @@ define base_title in app/helpers/application_helper.rb instead
       	# using Capybara page variable to test that resulting page has the right content
       page.should have_content('Sample App')
     end
-=end
+  =end
     
     it "should have the h1 'Home'" do
-      visit '/static_pages/home'
+      # Instead of using hard-code address can use desire named route
+      #visit '/static_pages/home'
+      #visit root_path
       page.should have_selector('h1', :text => 'Home')
     end
 
     # it "should have the title 'Home'" do
     it "should have the base title" do
-      visit '/static_pages/home'
+      #visit root_path
        # have_selector method checks for an HTML element (selector/title in below example) with the given content
       page.should have_selector('title',
                         # => uses a symbol as the key, so content listed below does not need to be exact match, can use "| Home"
@@ -34,11 +45,12 @@ define base_title in app/helpers/application_helper.rb instead
     end
 
     it "should not have a custom page title" do
-      visit '/static_pages/home'
+      #visit root_path
       page.should_not have_selector('title', :text => '| Home')
     end
 
   end
+=end
 
   describe "Help page" do
 =begin
@@ -49,12 +61,12 @@ define base_title in app/helpers/application_helper.rb instead
 =end
 
      it "should have the h1 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_selector('h1', :text => 'Help')
     end 
 
      it "should have the title 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_selector('title',
                         # need to remove #{base_title} as we are no longer using it at the top
                         # :text => "#{base_title} | Help")
@@ -71,13 +83,13 @@ define base_title in app/helpers/application_helper.rb instead
 =end
 
     it "should have the h1 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_selector('h1', :text => 'About Us')
    end
 
 
     it "should have the title 'About Us'" do
-    visit '/static_pages/about'
+    visit about_path
     page.should have_selector('title',
                       :text => "About Us")
     end
@@ -86,15 +98,14 @@ define base_title in app/helpers/application_helper.rb instead
   describe "Contact page" do
 
     it "should have the h1 'Contact'" do
-      visit '/static_pages/contact'
+      visit contact_path
       page.should have_selector('h1', :text => 'Contact')
    end
 
 
     it "should have the title 'Contact'" do
-    visit '/static_pages/contact'
-    page.should have_selector('title',
-                      :text => "Contact")
+      visit contact_path
+      page.should have_selector('title', text: "Contact")
     end
   end
 
