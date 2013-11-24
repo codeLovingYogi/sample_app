2b3c8557f900fbdfb44a52abe03b2945b85259b7
Felice H <felichay@outlook.com>
2013-11-24 00:56:28 -0500

    Make basic User model (including secure passwords)

---
+++ app/models/user.rb
@@ -0,0 +1,30 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class User < ActiveRecord::Base
+	#by default all model attributes are accessible (modifiable by outside users, should include attr_accessible line to prevent security hole)	
+  attr_accessible :email, :name, :password, :password_confirmation
+  #attr_accessible(:name, :;email)
+  has_secure_password
+
+  before_save { |user| user.email = email.downcase }
+
+  validates :name, presence: true, length: { maximum: 50 }
+  #validates(:name, presence:true)
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
+
+  
+  
+end
